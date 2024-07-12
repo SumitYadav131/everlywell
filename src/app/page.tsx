2bb64cca1d20796072b10a578b0c7d94f3c008b3
Sumit Yadav <sumit@mydevitsolutions.com>
2024-07-12 13:00:47 +0530

    for production experiment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
 'use client'
 
-
 import React from 'react';
-import { AppBar, Toolbar, Typography, Drawer, List, ListItem, ListItemText, Container, Grid } from '@mui/material';
-import Box from '@mui/material/Box';
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-
 
 const Home = () => {
   return (
     <div>
-     <h1>home page</h1>
+      <h1>home page</h1>
     </div>
   );
 };
