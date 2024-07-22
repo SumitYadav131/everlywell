a27b6bd514b6b0280d419799ce43cc86eba8e80d
Sumit Yadav <sumit@mydevitsolutions.com>
2024-07-22 16:54:18 +0530

    edit in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,17 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-  const isPublicPath = path === '/login' || path === '/signup';
-  const token = request.cookies.get("jwt")?.value || '';
+  // const path = request.nextUrl.pathname;
+  // const isPublicPath = path === '/login' || path === '/signup';
+  // const token = request.cookies.get("jwt")?.value || '';
 
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL('/admin/dashboard', request.nextUrl));
-  }
+  // if (isPublicPath && token) {
+  //   return NextResponse.redirect(new URL('/admin/dashboard', request.nextUrl));
+  // }
 
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL('/login', request.nextUrl));
-  }
+  // if (!isPublicPath && !token) {
+  //   return NextResponse.redirect(new URL('/login', request.nextUrl));
+  // }
 }
 
 // See "Matching Paths" below to learn more
